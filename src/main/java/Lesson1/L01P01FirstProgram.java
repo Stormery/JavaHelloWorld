322b8870b290c3d6206796302a4d29bca6c5e67f
Ayo-PC\Ayo <raf.urb89@gmail.com>
2018-02-07 15:10:19 +0000

    JavaFirst ver.1.01 added L01P01FirstProgram.java

---
+++ src/main/java/Lesson1/L01P01FirstProgram.java
@@ -0,0 +1,31 @@
+package Lesson1;
+
+
+/**
+ * Class L01P01FirstProgram  displays text in the console.
+ * <br><br>
+ *
+ * Class implements only main method:
+ * <ul>
+ *     <li>main method </li>
+ * </ul>
+ *
+ * @version 21.01.2018
+ * @author Rafał Urbański
+ */
+public class L01P01FirstProgram {
+    /**
+     *The entry main() method.
+     * <ul>
+     *      <li><p><code>System.out.println</code> - print data in console</p></li>
+     * </ul>
+     * @param args Args
+     */
+    public static void main(String[] args){
+        System.out.println("To jest mój pierwszy program napisany w języku Java");
+        System.out.println("i zbudowany za pomocą środowiska MAVEN");
+        System.out.println("Limerick");
+        System.out.println("23.01.2018 12:30");
+        System.out.println("Rafal Urbanski");
+    }
+}
---
+++ src/main/java/Lesson1/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Lesson1 is a group of Java classes using variables in the scope of their declaration, assignment and display.
+ */
+package Lesson1;
+
+//rest of the file is empty
